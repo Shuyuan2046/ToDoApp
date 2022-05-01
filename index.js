3d32c300e54981814f6607851ddb3ff6ff1dc3a6
Shuyuan Wang <wangsy.9932@gmail.com>
2022-05-01 17:12:41 -0400

    modify index

--- index.js
+++ index.js
@@ -12,8 +12,6 @@ app.use("./static", express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 //connection to db
-//mongoose.set("useFindAndModify", false);
-
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
     console.log("Connected to db!");
     app.listen(3000, () => console.log("Server Up and running"));
